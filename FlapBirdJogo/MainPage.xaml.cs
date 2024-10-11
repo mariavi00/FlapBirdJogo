e5cd4f979069703630469059fdc2681290cc0795
FlapBirdJogo <mavi.gouveia678@gmail.com>
2024-10-11 11:02:08 -0300

    11/10

--- FlapBirdJogo/MainPage.xaml.cs
+++ FlapBirdJogo/MainPage.xaml.cs
@@ -3,12 +3,34 @@
 public partial class MainPage : ContentPage
 {
 	const int gravidade=30;
-	const int tempoEntreFrames=100;
-	bool estaMorto=false;
+	const int tempoEntreFrames=50;
+	bool estaMorto=true;
+	double larguraJanela=0;
+	double alturaJanela=0;
+	int velocidade=20;
 
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		larguraJanela=w;
+		alturaJanela=h;
+	}
+
+	void GerenciaCanos()
+	{
+		ImagemCanoCima.TranslationX-=velocidade;
+		ImagemCanoBaixo.TranslationX-=velocidade;
+
+		if(ImagemCanoBaixo.TranslationX<-larguraJanela)
+		{
+			ImagemCanoBaixo.TranslationX=100;
+			ImagemCanoCima.TranslationX=100;
+		}
 	}
 	async Task Desenhar()
 	{
@@ -16,16 +38,24 @@ public partial class MainPage : ContentPage
 		{
 			AplicaGravidade();
 			await Task.Delay(tempoEntreFrames);
+
+			GerenciaCanos();
 		}
 	}
 	void AplicaGravidade()
 	{
 		ImagemFirebird.TranslationY+=gravidade;
 	}
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
+	void GameOverClicado(object s, TappedEventArgs a)
+	{
+		frameGameOver.IsVisible=false;
+		Inicializar();
 		Desenhar();
-    }
+	}
+	void Inicializar()
+	{
+		estaMorto=false;
+		ImagemFirebird.TranslationY=0;
+	}
 }
 

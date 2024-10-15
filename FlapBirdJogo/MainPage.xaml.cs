cfe777e3473dd3e4e5bdb9c883b0022f5471938b
FlapBirdJogo <mavi.gouveia678@gmail.com>
2024-10-15 11:04:54 -0300

    15/10

--- FlapBirdJogo/MainPage.xaml.cs
+++ FlapBirdJogo/MainPage.xaml.cs
@@ -8,6 +8,10 @@ public partial class MainPage : ContentPage
 	double larguraJanela=0;
 	double alturaJanela=0;
 	int velocidade=20;
+	const int maxTempoPulando=3;
+	int tempoPulando=0;
+	bool estaPulando=false;
+	const int forcaPulo=60;
 
 	public MainPage()
 	{
@@ -32,19 +36,64 @@ public partial class MainPage : ContentPage
 			ImagemCanoCima.TranslationX=100;
 		}
 	}
+	void AplicaPulo()
+	{
+		ImagemFirebird.TranslationY-=forcaPulo;
+		tempoPulando++;
+		if(tempoPulando>=maxTempoPulando)
+		{
+			estaPulando=false;
+			tempoPulando=0;
+		}
+	}
 	async Task Desenhar()
 	{
 		while(!estaMorto)
 		{
+			if(estaPulando)
+			AplicaPulo();
+		else
 			AplicaGravidade();
-			await Task.Delay(tempoEntreFrames);
-
 			GerenciaCanos();
+			if(VerificaColisao())
+			{
+				estaMorto=true;
+				frameGameOver.IsVisible=true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
 		}
 	}
 	void AplicaGravidade()
 	{
 		ImagemFirebird.TranslationY+=gravidade;
+	}
+	bool VerificaColisaoTeto()
+	{
+		var minY=-alturaJanela/2;
+		if (ImagemFirebird.TranslationY<=minY)
+			return true;
+		else
+			return false;
+	}
+	bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela/2 - ImagemChao.HeightRequest;
+		if (ImagemFirebird.TranslationY>= maxY)
+			return true;
+		else
+			return false;
+	}
+	bool VerificaColisao()
+	{
+		if(!estaMorto)
+		{
+			if(VerificaColisaoTeto()|| VerificaColisaoChao())
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	void GameOverClicado(object s, TappedEventArgs a)
 	{
